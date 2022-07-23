fd7dbd5d04f7e628b3c0d724d06c8ef7193a4670
Kunhee Kim <kunheekim@korea.ac.kr>
2022-07-23 10:41:44 +0900

    Feat: Bottom Navigation Bar

---
+++ lib/run_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RunPage extends StatelessWidget {
+  const RunPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Run Page');
+  }
+}
