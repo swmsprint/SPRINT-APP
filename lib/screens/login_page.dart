76efb41b42273bc5e92b189bd15d67aaef3668ce
Kunhee Kim <kunheekim@korea.ac.kr>
2022-10-12 23:57:16 +0900

    feat: Sign In with Google

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,6 +3,7 @@ import 'package:sprint/widgets/login_page/carousel.dart';
 import 'package:sprint/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:developer';
 
 class LoginPage extends StatelessWidget {
@@ -39,7 +40,8 @@ class LoginPage extends StatelessWidget {
                 height: MediaQuery.of(context).size.height * 0.07,
                 child: IconButton(
                   icon: Image.asset('assets/images/login/google.png'),
-                  onPressed: () {
+                  onPressed: () async {
+                    await signInWithGoogle();
                     Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(
@@ -87,6 +89,31 @@ class LoginPage extends StatelessWidget {
     // not match the nonce in `appleCredential.identityToken`, sign in will fail.
     final result =
         await FirebaseAuth.instance.signInWithCredential(oauthCredential);
+    log("$result");
+
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = auth.currentUser!;
+    final uid = user.uid;
+    log("$uid");
+    return result;
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    final result = await FirebaseAuth.instance.signInWithCredential(credential);
     log("$result");
 
     final FirebaseAuth auth = FirebaseAuth.instance;
